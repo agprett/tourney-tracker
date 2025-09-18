445c28461f671413ecf59d04a4d973593875b203
agprett <prettale4@gmail.com>
2025-09-17 19:58:24 -0500

    updating with most recent changes

--- client/Components/Home/TourneyRow.jsx
+++ client/Components/Home/TourneyRow.jsx
@@ -1,6 +1,22 @@
 import { VisibilityOutlined, Settings, DeleteForeverOutlined } from "@mui/icons-material"
+import { useNavigate } from "react-router"
 
 function TourneyRow () {
+  const navigate = useNavigate()
+
+  
+
+  const viewTournament = () =>  {
+    navigate('/tournament')
+  }
+
+  const viewSettings = () => {
+    console.log('Will edit some settings maybe')
+  }
+
+  const deleteTournament = () => {
+    console.log('Bring up delete confirm')
+  }
 
   return (
     <tr>
@@ -9,9 +25,9 @@ function TourneyRow () {
       <td>Players</td>
       <td>Format</td>
       <td>
-        <button className="green"><VisibilityOutlined /></button>
-        <button className="meh"><Settings /></button>
-        <button className="red"><DeleteForeverOutlined /></button>
+        <button className="green" onClick={viewTournament} ><VisibilityOutlined /></button>
+        <button className="meh" onClick={viewSettings}><Settings /></button>
+        <button className="red" onClick={deleteTournament}><DeleteForeverOutlined /></button>
       </td>
     </tr>
   )
--- client/Components/Tourney/Tourney.jsx
+++ client/Components/Tourney/Tourney.jsx
@@ -1,8 +1,22 @@
 function Tourney () {
 
   return (
-    <div>
-      Tourney
+    <div id="tournament-details">
+      <header className="tourney-details-header">
+        <h3>Tourney name</h3>
+        <p>Tourney Format</p>
+      </header>
+
+      <div className="tabs">
+        <button className="tab-buttons tab-buttons-active">Players</button>
+        <button className="tab-buttons">Rounds</button>
+        <button className="tab-buttons">Standings</button>
+        <button className="tab-buttons">Settings</button>
+      </div>
+
+      <div className="tabs-content">
+        Content
+      </div>
     </div>
   )
 }
